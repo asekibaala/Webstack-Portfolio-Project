57dd7f98de450242cd8de5a63fe622d931d3779b
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-03-29 14:35:01 +0300

    fixed security Issue and removed firebase keys

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQNF4-3d1SZwyrLRk5N4b7-175pjT2BO8",
-  authDomain: "house-marketplace-app-706c3.firebaseapp.com",
-  projectId: "house-marketplace-app-706c3",
-  storageBucket: "house-marketplace-app-706c3.appspot.com",
-  messagingSenderId: "629576441165",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
   appId: "1:629576441165:web:00bf7856ed4f6c6e49ab04",
 };
 
